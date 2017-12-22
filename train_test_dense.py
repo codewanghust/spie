7c8c7158cf07c51327b1334a85ad13e5276e7356
LELE CHEN <lele.chen@rochester.edu>
2017-12-22 15:01:34 -0500

    multi cpu

--- train_test_dense.py
+++ train_test_dense.py
@@ -47,7 +47,7 @@ def parse_inputs():
     parser.add_argument('-D', '--down-factor', dest='dfactor', type=int, default=500)
     parser.add_argument('-n', '--num-filters', action='store', dest='n_filters', nargs='+', type=int, default=[32])
     parser.add_argument('-e', '--epochs', action='store', dest='epochs', type=int, default=1)
-    parser.add_argument('-q', '--queue', action='store', dest='queue', type=int, default=10)
+    parser.add_argument('-q', '--queue', action='store', dest='queue', type=int, default=40)
     parser.add_argument('-u', '--unbalanced', action='store_false', dest='balanced', default=True)
     parser.add_argument('-s', '--sequential', action='store_true', dest='sequential', default=False)
     parser.add_argument('--preload', action='store_true', dest='preload', default=False)
@@ -451,6 +451,7 @@ def main():
             validation_steps=val_steps_per_epoch,
             workers = 40,
             max_q_size=queue,
+            , use_multiprocessing=True,
             epochs=epochs
         )
         net.save(net_name)
