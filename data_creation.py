87ec3126b76c8c399b37be79beb2e5f2ad5df2ea
lelechen63 <lchen63@ur.rochester.edu>
2017-12-24 00:49:38 -0500

    binary

--- data_creation.py
+++ data_creation.py
@@ -192,7 +192,30 @@ def get_xy(
         y = keras.utils.to_categorical(np.copy(y).astype(dtype=np.bool), num_classes=2)
     x = np.transpose(x, axes=[0, 2, 3, 4, 1])
     return x, y
+class threadsafe_iter:
+    """Takes an iterator/generator and makes it thread-safe by
+    serializing call to the `next` method of given iterator/generator.
+    """
+    def __init__(self, it):
+        self.it = it
+        self.lock = threading.Lock()
 
+    def __iter__(self):
+        return self
+
+    def next(self):
+        with self.lock:
+            return self.it.next()
+
+
+def threadsafe_generator(f):
+    """A decorator that takes a generator function and makes it thread-safe.
+    """
+    def g(*a, **kw):
+        return threadsafe_iter(f(*a, **kw))
+    return g
+
+@threadsafe_generator
 
 def load_patch_batch_train(
         image_names,
