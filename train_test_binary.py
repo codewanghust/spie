419d85eb65ce31e746ade9edfbe730c1c4e6761a
lelechen63 <lchen63@ur.rochester.edu>
2017-12-24 00:20:30 -0500

    binary

--- train_test_binary.py
+++ train_test_binary.py
@@ -478,7 +478,7 @@ def main():
         ),
         steps_per_epoch=train_steps_per_epoch,
         validation_steps=val_steps_per_epoch,
-        workers=30,
+        workers=40,
         max_q_size=queue,
         epochs=epochs
     )
