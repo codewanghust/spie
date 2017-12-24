b07be079fce0dc5a378ce0c67216dc21d2df6b02
lelechen63 <lchen63@ur.rochester.edu>
2017-12-23 22:10:37 -0500

    4 -> num-class

--- train_test_dense.py
+++ train_test_dense.py
@@ -55,7 +55,7 @@ def dense_model(patch_size, num_classes):
 
     tumor = Conv3D(2, kernel_size=1, strides=1, name='tumor')(flair)
     core = Conv3D(3, kernel_size=1, strides=1, name='core')(t2)
-    enhancing = Conv3D(4, kernel_size=1, strides=1, name='enhancing')(t1)
+    enhancing = Conv3D(num_classes, kernel_size=1, strides=1, name='enhancing')(t1)
     net = Model(inputs=merged_inputs, outputs=[tumor, core, enhancing])
     return net
 
