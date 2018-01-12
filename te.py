ef9f8f8a9fd425feed7288cefeeae67844a9e205
lelechen63 <lchen63@ur.rochester.edu>
2018-01-11 22:28:00 -0500

    gg

--- te.py
+++ te.py
@@ -12,6 +12,8 @@ def parse_inputs():
     parser.add_argument('-mn', '--model_name', dest='model_name', type=str, default='dense24')
     parser.add_argument('-r', '--root', dest='root', type=str, default='')
     parser.add_argument('-i', '--inx', dest='inx', type=int, default=1)
+    parser.add_argument('-t', '--txt', dest='txt', type=str, default='/home/lchen63/project/spie/test.txt')
+
     return vars(parser.parse_args())
 
 
@@ -19,7 +21,11 @@ options = parse_inputs()
 model_name = options['model_name']
 root = options['root']
 inx = options['inx']
-
+txt  = options['txt']
+name  = []
+with open(txt) as f:
+	for line in f:
+	    name.append(line[:-1])
 # dice_whole =  np.load( root +'/' +   model_name + '_dice_whole.npy')
 # dice_core = np.expand_dims(np.load( root + '/' + model_name + '_dice_core.npy')[:,1],axis = 1)
 # dice_et = np.expand_dims(np.load(root + '/' +  model_name + '_dice_enhance.npy')[:,1],axis = 1)
@@ -44,7 +50,10 @@ print (c[18:].mean(axis=0))
 print ('================================================')
 
 
-c=  b[np.multiply(b[:,1],b[:,2],b[:,3]).argsort()]
-print (c[18:])
-print (c[18:].mean(axis=0))
-print ('================================================')
+
+
+new = name[b[:,inx].argsort()]
+
+for i in range(len(new)):
+	print new[i]
+
